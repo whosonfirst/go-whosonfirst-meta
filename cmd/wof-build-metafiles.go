bc854043d60eaeda3695e3bfe3e7d812d24c3bd0
thisisaaronland <straup+github@gmail.com>
2018-04-24 16:52:12 -0700

    start wiring in go-wof-index code; break everything

--- cmd/wof-build-metafiles.go
+++ cmd/wof-build-metafiles.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+       "context"
 	"flag"
 	"fmt"
 	"github.com/facebookgo/atomicfile"
-	"github.com/tidwall/gjson"
-	"github.com/whosonfirst/go-whosonfirst-crawl"
 	"github.com/whosonfirst/go-whosonfirst-csv"
+	"github.com/whosonfirst/go-whosonfirst-geojson-v2/feature"
+	"github.com/whosonfirst/go-whosonfirst-index"
+	"github.com/whosonfirst/go-whosonfirst-index/utils"
 	"github.com/whosonfirst/go-whosonfirst-meta"
 	"github.com/whosonfirst/go-whosonfirst-placetypes/filter"
 	"github.com/whosonfirst/go-whosonfirst-repo"
-	"github.com/whosonfirst/go-whosonfirst-uri"
-	_ "io"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"runtime"
 	"sort"
 	"strings"
 	"sync"
@@ -26,82 +26,20 @@ import (
 
 func main() {
 
-	repo_path := flag.String("repo", "", "Where to read data (to create metafiles) from. If empty then the code will assume the current working directory.")
+	mode := flag.String("mode", "repo", "Where to read data (to create metafiles) from. If empty then the code will assume the current working directory.")
 	out := flag.String("out", "", "Where to store metafiles. If empty then assume metafile are created in a child folder of 'repo' called 'meta'.")
 
-	procs := flag.Int("processes", runtime.NumCPU()*2, "The number of concurrent processes to use.")
 	limit := flag.Int("open-filehandles", 512, "The maximum number of file handles to keep open at any given moment.")
 
 	str_placetypes := flag.String("placetypes", "", "A comma-separated list of placetypes that meta files will be created for. All other placetypes will be ignored.")
 	str_roles := flag.String("roles", "", "Role-based filters are not supported yet.")
 	str_exclude := flag.String("exclude", "", "A comma-separated list of placetypes that meta files will not be created for.")
 
+	timings := flag.Bool("timings", false, "...")
+
+	procs := flag.Int("processes", 0, "The number of concurrent processes to use. THIS FLAG HAS BEEN DEPRECATED")
+
 	flag.Parse()
-
-	runtime.GOMAXPROCS(*procs)
-
-	if *repo_path == "" {
-
-		cwd, err := os.Getwd()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		*repo_path = cwd
-	}
-
-	abs_repo, err := filepath.Abs(*repo_path)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	info, err := os.Stat(abs_repo)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if !info.IsDir() {
-		log.Fatal(fmt.Sprintf("Invalid repo directory (%s)", abs_repo))
-	}
-
-	r, err := repo.NewDataRepoFromPath(abs_repo)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var abs_meta string
-
-	if *out == "" {
-		abs_meta = filepath.Join(abs_repo, "meta")
-	} else {
-		abs_meta = *out
-	}
-
-	info, err = os.Stat(abs_meta)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if !info.IsDir() {
-		log.Fatal(fmt.Sprintf("Invalid meta directory (%s)", abs_meta))
-	}
-
-	abs_data := filepath.Join(abs_repo, "data")
-
-	info, err = os.Stat(abs_data)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if !info.IsDir() {
-		log.Fatal(fmt.Sprintf("Invalid data directory (%s)", abs_data))
-	}
 
 	placetypes := make([]string, 0)
 	roles := make([]string, 0)
@@ -130,7 +68,6 @@ func main() {
 	throttle := make(chan bool, *limit)
 
 	for i := 0; i < *limit; i++ {
-
 		throttle <- true
 	}
 
@@ -156,7 +93,7 @@ func main() {
 
 	wg := new(sync.WaitGroup)
 
-	callback := func(path string, info os.FileInfo) error {
+	cb := func(fh io.Reader, ctx context.Context, args ...interface{}) error {
 
 		atomic.AddInt32(&pending, 1)
 		// log.Printf("pending %d scheduled %d\n", pending, scheduled)
@@ -173,38 +110,27 @@ func main() {
 			wg.Done()
 		}()
 
-		if info.IsDir() {
+		path, err := index.PathForContext(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		ok, err := utils.IsPrincipalWOFRecord(fh, ctx)
+
+		if err != nil {
+			return err
+		}
+
+		if !ok {
 			return nil
 		}
 
-		wof, err := uri.IsWOFFile(path)
+		f, err := feature.LoadFeatureFromReader(fh)
 
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
-
-		if !wof {
-			return nil
-		}
-
-		alt, err := uri.IsAltFile(path)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if alt {
-			return nil
-		}
-
-		fh, err := os.Open(path)
-
-		if err != nil {
-			msg := fmt.Sprintf("Failed to open %s because %s (%d open filehandles)", path, err, atomic.LoadInt32(&open))
-			log.Fatal(msg)
-		}
-
-		defer fh.Close()
 
 		atomic.AddInt32(&open, 1)
 		defer atomic.AddInt32(&open, -1)
@@ -215,16 +141,14 @@ func main() {
 			log.Fatal(err)
 		}
 
-		placetype := gjson.GetBytes(feature, "properties.wof:placetype").String()
+		placetype := f.Placetype()
 
 		allow, err := placetype_filter.AllowFromString(placetype)
 
 		if err != nil {
 			log.Println(fmt.Sprintf("Unable to validate placetype (%s) for %s", placetype, path))
-			// log.Fatal(err)
+			return err
 		}
-
-		// log.Printf("Allow %s : %t\n", placetype, allow)
 
 		if !allow {
 			return nil
@@ -233,7 +157,7 @@ func main() {
 		row, err := meta.FeatureToRow(feature)
 
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		mu.Lock()
@@ -278,17 +202,36 @@ func main() {
 		return nil
 	}
 
-	t1 := time.Now()
-
-	cr := crawl.NewCrawler(abs_data)
-	err = cr.Crawl(callback)
-
-	wg.Wait()
-
-	t2 := time.Since(t1)
-	log.Printf("time to dump %d features: %v\n", count, t2)
+	i, err := index.NewIndexer(*mode, cb)
 
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	t1 := time.Now()
+
+	for _, path := range flag.Args() {
+
+		ta := time.Now()
+
+		err := i.IndexPath(path)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tb := time.Since(ta)
+
+		if *timings {
+			log.Printf("time to prepare %s %v\n", path, tb)
+		}
+
+	}
+
+	t2 := time.Since(t1)
+
+	if *timings {
+		c := atomic.LoadInt32(&count)
+		log.Printf("time to prepare all %d records %v\n", c, t2)
 	}
 }
